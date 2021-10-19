7d3e8934a322b524bcf88b0971c387301826670c
Philippe Chlenski <p@chlenski.com>
2021-10-19 16:18:51 -0400

    Refactor solver into db.py

--- src/solver.py
+++ src/solver.py
@@ -200,194 +200,194 @@ def solver(
 
     return results
 
-def solve_genome(
-    genome_id : str,
-    sample_id : str,
-    database : pd.DataFrame,
-    otus : pd.DataFrame,
-    true_ptrs : pd.DataFrame,
-    regularization : float = 0) -> dict:
-    """
-    Given a genome ID, a DB, and some coverages, estimate the PTR.
+# def solve_genome(
+#     genome_id : str,
+#     sample_id : str,
+#     database : pd.DataFrame,
+#     otus : pd.DataFrame,
+#     true_ptrs : pd.DataFrame,
+#     regularization : float = 0) -> dict:
+#     """
+#     Given a genome ID, a DB, and some coverages, estimate the PTR.
 
-    Args:
-    -----
-    genome_id:
-        String. Genome ID as provided by md5_to_genomes().
-    sample_id:
-        Integer or string. Sample number for the given genome.
-    database:
-        Pandas DataFrame. A matrix of 16S positions and sequences.
-    otus:
-        Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
-    true_ptrs:
-        Pandas DataFrame. A matrix of true PTR values, if known.
-    regularization:
-        Float. L2-regularization penalty applied to m and b.
+#     Args:
+#     -----
+#     genome_id:
+#         String. Genome ID as provided by md5_to_genomes().
+#     sample_id:
+#         Integer or string. Sample number for the given genome.
+#     database:
+#         Pandas DataFrame. A matrix of 16S positions and sequences.
+#     otus:
+#         Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
+#     true_ptrs:
+#         Pandas DataFrame. A matrix of true PTR values, if known.
+#     regularization:
+#         Float. L2-regularization penalty applied to m and b.
 
-    Returns:
-    --------
-    A dict with keys 'genome', 'sample', 'ptr', 'true_ptr' to be appended to a results DB.
+#     Returns:
+#     --------
+#     A dict with keys 'genome', 'sample', 'ptr', 'true_ptr' to be appended to a results DB.
 
-    Raises:
-    -------
-    TODO
-    """
-    # Build up x_values
-    db_matched = database[genome_id]
-    x_positions = db_matched['16s_position'] / db_matched['size']
+#     Raises:
+#     -------
+#     TODO
+#     """
+#     # Build up x_values
+#     db_matched = database[genome_id]
+#     x_positions = db_matched['16s_position'] / db_matched['size']
 
-    # Build up mappings
-    md5s_matched = db_matched['16s_md5']
-    mapping = {}
-    idx = 0
-    # TODO: vectorize this
-    for md5 in md5s_matched:
-        if md5 not in mapping:
-            mapping[md5] = idx
-            idx += 1
-    x_mapping = [mapping[x] for x in md5s_matched]
+#     # Build up mappings
+#     md5s_matched = db_matched['16s_md5']
+#     mapping = {}
+#     idx = 0
+#     # TODO: vectorize this
+#     for md5 in md5s_matched:
+#         if md5 not in mapping:
+#             mapping[md5] = idx
+#             idx += 1
+#     x_mapping = [mapping[x] for x in md5s_matched]
 
-    # Sort md5s by their index
-    md5s = [None for _ in mapping]
-    for md5 in mapping:
-        md5s[mapping[md5]] = md5
+#     # Sort md5s by their index
+#     md5s = [None for _ in mapping]
+#     for md5 in mapping:
+#         md5s[mapping[md5]] = md5
 
-    # Build up coverages
-    coverages = otus[sample_id].reindex(md5s)
+#     # Build up coverages
+#     coverages = otus[sample_id].reindex(md5s)
 
-    # Send to solver
-    results = solver(
-        x_values=x_positions, 
-        mappings=x_mapping, 
-        coverages=coverages, 
-        regularization=regularization
-    )
+#     # Send to solver
+#     results = solver(
+#         x_values=x_positions, 
+#         mappings=x_mapping, 
+#         coverages=coverages, 
+#         regularization=regularization
+#     )
 
-    # Append output to PTRs dataframe
-    m = results[0]
-    b = results[1]
-    peak = np.exp2(b)
-    trough = np.exp2(m * 0.5 + b)
-    ptr = peak / trough
+#     # Append output to PTRs dataframe
+#     m = results[0]
+#     b = results[1]
+#     peak = np.exp2(b)
+#     trough = np.exp2(m * 0.5 + b)
+#     ptr = peak / trough
 
-    # Get true PTR
-    if true_ptrs is not None:
-        try:
-            true_ptr = true_ptrs.loc[genome_id, sample_id]
-        except KeyError as e:
-            true_ptr = np.nan
-            print(f"True PTR evaluation: Bypassing KeyError: {e}")
-    else:
-        true_ptr = None
+#     # Get true PTR
+#     if true_ptrs is not None:
+#         try:
+#             true_ptr = true_ptrs.loc[genome_id, sample_id]
+#         except KeyError as e:
+#             true_ptr = np.nan
+#             print(f"True PTR evaluation: Bypassing KeyError: {e}")
+#     else:
+#         true_ptr = None
 
-    return {"genome" : genome_id, "sample" : sample_id, "ptr" : ptr, "true_ptr" : true_ptr}
+#     return {"genome" : genome_id, "sample" : sample_id, "ptr" : ptr, "true_ptr" : true_ptr}
 
-def solve_sample(
-    sample_id : str,
-    database : pd.DataFrame,
-    otus : pd.DataFrame,
-    true_ptrs : pd.DataFrame,
-    regularization : float = 0) -> list:
-    """
-    Given a sample name, solve all available 16S systems.
+# def solve_sample(
+#     sample_id : str,
+#     database : pd.DataFrame,
+#     otus : pd.DataFrame,
+#     true_ptrs : pd.DataFrame,
+#     regularization : float = 0) -> list:
+#     """
+#     Given a sample name, solve all available 16S systems.
 
-    Args:
-    -----
-    sample_id:
-        String. The name of a given sample. Used for indexing into OTU matrix.
-    database:
-        RnaDB object. A matrix of 16S positions and sequences.
-    otus:
-        Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
-    true_ptrs:
-        Pandas DataFrame. A matrix of true PTR values, if known.
-    regularization:
-        Float. L2-regularization penalty applied to m and b.
+#     Args:
+#     -----
+#     sample_id:
+#         String. The name of a given sample. Used for indexing into OTU matrix.
+#     database:
+#         RnaDB object. A matrix of 16S positions and sequences.
+#     otus:
+#         Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
+#     true_ptrs:
+#         Pandas DataFrame. A matrix of true PTR values, if known.
+#     regularization:
+#         Float. L2-regularization penalty applied to m and b.
 
-    Returns:
-    --------
-    A list of dicts (solve_genome outputs) to be appended to an output DB.
+#     Returns:
+#     --------
+#     A list of dicts (solve_genome outputs) to be appended to an output DB.
 
-    Raises:
-    -------
-    TODO
-    """
-    genomes = []
-    out = []
-    # Build up lists of genomes and md5s
-    for md5 in otus.index:
-        if otus.loc[md5, sample_id] > 0:
-            match, coll = database.md5_to_genomes(md5)
+#     Raises:
+#     -------
+#     TODO
+#     """
+#     genomes = []
+#     out = []
+#     # Build up lists of genomes and md5s
+#     for md5 in otus.index:
+#         if otus.loc[md5, sample_id] > 0:
+#             match, coll = database.md5_to_genomes(md5)
 
-            # Skip collisions for now... these are overdetermined
-            if coll:
-                print(f"Collision for {md5}. Skipping this sequence...")
-            else:
-                genomes += match
+#             # Skip collisions for now... these are overdetermined
+#             if coll:
+#                 print(f"Collision for {md5}. Skipping this sequence...")
+#             else:
+#                 genomes += match
 
-    for genome_id in set(genomes):
-        result = solve_genome(
-            genome_id, sample_id, 
-            database=database, 
-            otus=otus, 
-            true_ptrs=true_ptrs, 
-            regularization=regularization
-        )
-        out.append(result)
+#     for genome_id in set(genomes):
+#         result = solve_genome(
+#             genome_id, sample_id, 
+#             database=database, 
+#             otus=otus, 
+#             true_ptrs=true_ptrs, 
+#             regularization=regularization
+#         )
+#         out.append(result)
 
-    return out
+#     return out
 
-def solve_matrix(
-    database : RnaDB,
-    otus : pd.DataFrame,
-    true_ptrs : pd.DataFrame = None,
-    max_error : float = np.inf,
-    regularization : float = 0) -> pd.DataFrame:
-    """
-    Given a 16S db, OTU read/abundance matrix, and true PTR values (optional), esimate PTRs.
+# def solve_matrix(
+#     database : RnaDB,
+#     otus : pd.DataFrame,
+#     true_ptrs : pd.DataFrame = None,
+#     max_error : float = np.inf,
+#     regularization : float = 0) -> pd.DataFrame:
+#     """
+#     Given a 16S db, OTU read/abundance matrix, and true PTR values (optional), esimate PTRs.
 
-    Args:
-    -----
-    database:
-        RnaDB object. A matrix of 16S positions and sequences.
-    otus:
-        Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
-    true_ptrs:
-        Pandas DataFrame. A matrix of true PTR values, if known.
-    max_error:
-        Float. Used to trim extreme error values (experimental).
-    regularization:
-        Float. L2-regularization penalty applied to m and b.
+#     Args:
+#     -----
+#     database:
+#         RnaDB object. A matrix of 16S positions and sequences.
+#     otus:
+#         Pandas DataFrame. A matrix of 16S OTU read/abundance counts.
+#     true_ptrs:
+#         Pandas DataFrame. A matrix of true PTR values, if known.
+#     max_error:
+#         Float. Used to trim extreme error values (experimental).
+#     regularization:
+#         Float. L2-regularization penalty applied to m and b.
 
-    Returns:
-    --------
-    A Pandas Dataframe with the following columns:
-    * genome: genome ID (taken from database)
-    * sample: sample ID (as given in OTU matrix)
-    * ptr: PTR (estimated by tool)
-    * true_ptr: PTR (provided in true_ptrs DataFrame)
-    * err: absolute error (capped at 5) between true and estimated PTR
+#     Returns:
+#     --------
+#     A Pandas Dataframe with the following columns:
+#     * genome: genome ID (taken from database)
+#     * sample: sample ID (as given in OTU matrix)
+#     * ptr: PTR (estimated by tool)
+#     * true_ptr: PTR (provided in true_ptrs DataFrame)
+#     * err: absolute error (capped at 5) between true and estimated PTR
 
-    Raises:
-    -------
-    TODO
-    """
-    out = pd.DataFrame(columns=["genome", "sample", "ptr", "true_ptr"])
+#     Raises:
+#     -------
+#     TODO
+#     """
+#     out = pd.DataFrame(columns=["genome", "sample", "ptr", "true_ptr"])
 
-    # For each column, build up x_values, mappings, coverages; send to solver
-    for column in otus.columns:
-        results = solve_sample(
-            column, 
-            database=database, 
-            otus=otus, 
-            true_ptrs=true_ptrs, 
-            regularization=regularization
-        )
-        out = out.append(results, ignore_index=True)
+#     # For each column, build up x_values, mappings, coverages; send to solver
+#     for column in otus.columns:
+#         results = solve_sample(
+#             column, 
+#             database=database, 
+#             otus=otus, 
+#             true_ptrs=true_ptrs, 
+#             regularization=regularization
+#         )
+#         out = out.append(results, ignore_index=True)
 
-    out['err'] = np.abs(out['ptr'] - out['true_ptr'])
-    # Cut off error threshold
-    out['err'] = out['err'].apply(lambda x: np.min([x, max_error]))
+#     out['err'] = np.abs(out['ptr'] - out['true_ptr'])
+#     # Cut off error threshold
+#     out['err'] = out['err'].apply(lambda x: np.min([x, max_error]))
 
-    return out
+#     return out
