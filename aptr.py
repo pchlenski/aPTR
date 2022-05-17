f2f361c8f1d34911372e57b721d3de932da1fd83
Philippe Chlenski <p@chlenski.com>
2022-05-17 14:17:18 -0400

    Sequence processing scripts

---
+++ aptr.py
@@ -0,0 +1,10 @@
+import sys
+from src.process_samples import process_samples
+from src.matrix_solver import OTUSolver
+
+if len(sys.argv) < 4:
+	raise ValueError("Not enough values!") 	
+
+_, path, adapter1, adapter2 = sys.argv
+
+process_samples(path, adapter1, adapter2)
